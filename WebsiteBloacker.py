3f67666c735198cdb6c3f926736ad3a61af3c59f
VineeS <51103512+VineeS@users.noreply.github.com>
2019-11-21 17:50:45 -0500

    Initial commit

---
+++ WebsiteBloacker.py
@@ -0,0 +1,11 @@
+import time
+
+hosts_path = r"/private/etc/hosts" # host path
+redirect = "120.0.0.1" # redirect server address
+
+website_list = ["www.facebook.com", "facebook.com"] # the website to be blocked
+while True:
+    print(1)
+    time.sleep(5) # this will print 1 in every 5 seconds
+
+    
