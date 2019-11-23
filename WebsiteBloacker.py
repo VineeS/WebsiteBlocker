64791cff564731ded8b455de5ea62fa0e993e193
VineeS <vinee@Vinees-MBP.fios-router.home>
2019-11-23 08:36:16 -0500

    comparing date and time

--- WebsiteBloacker.py
+++ WebsiteBloacker.py
@@ -1,11 +1,26 @@
 import time
+from datetime import datetime
 
 hosts_path = r"/private/etc/hosts" # host path
 redirect = "120.0.0.1" # redirect server address
 
 website_list = ["www.facebook.com", "facebook.com"] # the website to be blocked
+
+# >>> from datetime import datetime
+#>>> datetime.now()
+# output --- datetime.datetime(2019, 11, 23, 7, 51, 58, 2519)
+# check the date  
+
+#compare the datetime
+#datetime.now() < datetime(2019,11,23,7)
+#False
+
 while True:
-    print(1)
+    if datetime(datetime.now().year, datetime.now().month, datetime.now().day,8) < datetime.now() < datetime(datetime.now().year, datetime.now().month, datetime.now().day,16):
+        print("working hours !!!")
+    else:
+        print("Fun hours !!!")
+        
     time.sleep(5) # this will print 1 in every 5 seconds
 
     
